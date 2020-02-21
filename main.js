6352bdc961863b65e99f9f7a50dd9298a22e04cc
thelastmmt <gabrielbassinello@hotmail.com>
2020-02-20 22:59:03 -0300

    Vue now stores new user data in Contatlist key in LocalStorage using JSON Table now shows content dinamically Mounted gets all the localStorage data Watch keeps track os contact array changes and immediately update the localStorage without browser refreshing

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
+const STORAGE_KEY = 'contactList';
 var app = new Vue({
     el: '#app',
     data: {
-        errorMsg: false,
-        successMsg: false,
+        errorMsg: '',
+        successMsg: '',
+        showAddModal: false,
+        showEditModal: false,
+        showDeleteModal: false,
+        name: '',
+        email: '',
+        telephone: '',
+        contacts: [],
+    },
+    mounted(){
+        if(localStorage.getItem(STORAGE_KEY)){
+            try{
+                this.contacts = JSON.parse(localStorage.getItem(STORAGE_KEY))
+            } catch(e) {
+                errorMsg = e;
+                localStorage.removeItem(STORAGE_KEY);
+            }
+        }
+    },
+    watch: {
+        contacts: {
+            handler(){
+                console.log('Contacts changed');
+                localStorage.setItem(STORAGE_KEY, JSON.stringify(this.contacts))
+            },
+            deep: true,
+        },
+    },
+    methods: {
+        addContact(){
+            this.contacts.push({
+                id: this.contacts.length,
+                name: this.name,
+                email: this.email,
+                telephone: this.telephone});
+            this.name = '';
+            this.email = '';
+            this.telephone = '';
+        },
     }
 })
