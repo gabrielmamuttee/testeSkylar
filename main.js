29de3a49ce99910b3b648c1d3e2f11765eb965b9
thelastmmt <gabrielbassinello@hotmail.com>
2020-02-21 03:51:18 -0300

    Modals now have fade transitions Fixed a few bugs Confirmation dialog box added for contact deletion Success and error messages are now showed correctly Updating contacts array through editContacts functions Updated some bootstrap classes in the index.html

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var app = new Vue({
         showAddModal: false,
         showEditModal: false,
         showDeleteModal: false,
+        uid: '',
         name: '',
         email: '',
         telephone: '',
@@ -17,13 +18,14 @@ var app = new Vue({
             try{
                 this.contacts = JSON.parse(localStorage.getItem(STORAGE_KEY))
             } catch(e) {
-                errorMsg = e;
+                app.errorMsg = e;
                 localStorage.removeItem(STORAGE_KEY);
             }
         }
     },
     watch: {
         contacts: {
+            //Keep watching for changes in the contacts array, if anything changes, it automatically updates the contactList JSON array
             handler(){
                 console.log('Contacts changed');
                 localStorage.setItem(STORAGE_KEY, JSON.stringify(this.contacts))
@@ -32,6 +34,18 @@ var app = new Vue({
         },
     },
     methods: {
+        getContact(contactId){
+            this.uid = contactId;
+            this.name = this.contacts[contactId].name;
+            this.email = this.contacts[contactId].email;
+            this.telephone = this.contacts[contactId].telephone;
+        },
+        clearCurrentContact(){
+            this.uid = '';
+            this.name = '';
+            this.email = '';
+            this.telephone = '';
+        },
         addContact(){
             //If the contact array is null id = 0, if not, search the last item in array and sum + 1 into the id
             var identifier = '';
@@ -40,18 +54,28 @@ var app = new Vue({
             } else {
                 identifier = parseInt((this.contacts[this.contacts.length - 1].id + 1));
             }
-            console.log(identifier);
             this.contacts.push({
                 id: identifier,
                 name: this.name,
                 email: this.email,
                 telephone: this.telephone});
-            this.name = '';
-            this.email = '';
-            this.telephone = '';
+            app.clearCurrentContact();
+            app.successMsg = 'Contact added successfully!';
         },
         removeContact(contactId){
-            this.$delete(this.contacts, contactId);
+            if(confirm('Do you really want to delete ' + this.contacts[contactId].name + '?')){
+                this.$delete(this.contacts, contactId);
+                app.successMsg = 'Contact removed successfully!';
+            } else {
+                app.errorMsg = 'Delete canceled';
+            }
+            app.clearCurrentContact();
         },
+        editContact(){
+            this.contacts[this.uid].name = this.name;
+            this.contacts[this.uid].email = this.email;
+            this.contacts[this.uid].telephone = this.telephone;
+            app.clearCurrentContact();
+        }
     }
 })
